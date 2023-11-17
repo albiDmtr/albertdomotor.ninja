7982ce5e69fae0a8b18421d856415c544bad275b
Albert Domotor <domotor.albert@gmail.com>
2023-11-17 21:30:07 +0200

    viewport fixed

--- website/src/routes/Home.svelte
+++ website/src/routes/Home.svelte
@@ -17,7 +17,6 @@
 </script>
 <svelte:head>
   <title>Albert Dömötör – Software Engineer</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </svelte:head>
 <Cursor />
 <Menu />
